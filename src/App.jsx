750f92b3509d1a2f25982601f46f5d2720a5e083
aidynM <aidyn.meiramkhanuly@gmail.com>
2024-12-17 16:15:48 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import { Header } from './components/Header'
 import { QuizCart } from './components/QuizCart'
-import { ResultPage } from './components/ResultPage'
+import { ResultPage } from './components/ResultPage`'
 
 const App = () => {
 	return (
